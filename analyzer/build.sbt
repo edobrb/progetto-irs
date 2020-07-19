06596ded89cec6c10af7392ca7483bd9cbcbc10e
Edoardo Barbieri <edoardo.barbieri.96@gmail.com>
2020-07-19 08:57:53 -0700

    Add a concept for gathering the datas (project analyzer)

---
+++ analyzer/build.sbt
@@ -0,0 +1,7 @@
+name := "analyzer"
+
+version := "0.1"
+
+scalaVersion := "2.13.3"
+
+libraryDependencies += "com.typesafe.play" %% "play-json" % "2.9.0"
---
+++ analyzer/src/main/scala/Main.scala
@@ -0,0 +1,42 @@
+import java.io.File
+
+import play.api.libs.json._
+
+import scala.sys.process.{Process, ProcessLogger}
+import scala.util.{Failure, Success, Try}
+
+case class BooleanNetwork(functions: Seq[Seq[Boolean]],
+                          connections: Seq[Seq[Int]],
+                          inputs: Seq[Int],
+                          outputs: Seq[Int],
+                          overridden_output_functions: Option[Seq[Seq[Boolean]]])
+
+case class StepInfo(step: Int, id: String, boolean_network: Option[BooleanNetwork], states:Seq[Boolean], fitness:Double)
+
+object Main extends App {
+  implicit val bnFormat: OFormat[BooleanNetwork] = Json.format[BooleanNetwork]
+  implicit val siFormat: OFormat[StepInfo] = Json.format[StepInfo]
+
+  val outPipe: String => Unit = out => {
+    Try(Json.parse(out)) match {
+      case Failure(_) => println("Failed to parse json: " + out)
+      case Success(json) => Json.fromJson[StepInfo](json) match {
+        case JsSuccess(info, _) => handleInfo(info)
+        case JsError(_) => println("Failed to convert json: " + json)
+      }
+    }
+  }
+
+  val data = scala.collection.mutable.Map[(String, Int), StepInfo]()
+  def handleInfo(info:StepInfo): Unit = {
+    data += (info.id, info.step) -> info
+    if(info.id == "fb1") {
+      println(info.fitness+" "+info.boolean_network.isDefined)
+    }
+  }
+
+
+  Process(s"argos3 -c simulation.argos", new File("/home/edo/Desktop/progetto-irs")) run ProcessLogger(outPipe, print)
+
+
+}
