b420368b763f9731cd4348bb5809066f09a7149d
Edoardo Barbieri <edoardo.barbieri.96@gmail.com>
2020-11-01 11:45:29 -0800

    Add install utilities

---
+++ install-argos.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt-get install cmake libfreeimage-dev libfreeimageplus-dev qt5-default freeglut3-dev libxi-dev libxmu-dev liblua5.3-dev lua5.3 doxygen graphviz graphviz-dev asciidoc g++ git
+git clone https://github.com/ilpincy/argos3.git argos3
+cd argos3
+mkdir build_simulator
+cd build_simulator
+cmake -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX=/usr/local \
+        -DARGOS_BUILD_FOR=simulator \
+        -DARGOS_BUILD_NATIVE=ON \
+        -DARGOS_THREADSAFE_LOG=ON \
+        -DARGOS_DYNAMIC_LOADING=ON \
+        -DARGOS_USE_DOUBLE=ON \
+        -DARGOS_DOCUMENTATION=ON \
+        -DARGOS_INSTALL_LDSOCONF=ON \
+        ../src
+make doc
+sudo make install
+sudo ldconfig
+argos3 --version
