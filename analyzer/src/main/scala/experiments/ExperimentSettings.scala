9e562fa944c0649043ac0194a675e90744c8f2c3
Edoardo Barbieri <edoardo.barbieri.96@gmail.com>
2020-11-19 09:16:30 -0800

    Add a fourth experiment

--- analyzer/src/main/scala/experiments/ExperimentSettings.scala
+++ analyzer/src/main/scala/experiments/ExperimentSettings.scala
@@ -13,6 +13,7 @@ object ExperimentSettings {
   def apply(name: String): ExperimentSettings = Map[String, ExperimentSettings](
     "1" -> First,
     "2" -> Second,
-    "3" -> Third
+    "3" -> Third,
+    "4" -> Fourth
   )(name)
 }
